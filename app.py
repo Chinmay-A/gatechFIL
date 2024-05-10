fd2ec70c327a45e2ab9159243de52f728b719f55
Chinmay Amrutkar <93521349+Chinmay-A@users.noreply.github.com>
2024-05-11 02:40:13 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -7,15 +7,15 @@ server=app.server
 
 app.layout = html.Div([
     html.H1(children='GATECH-FIL RECRUITMENT TASK', style={'textAlign':'center'}),
-    dcc.Dropdown(['MSFT','GOOGL','AAPL'], 'MSFT', id='ticker'),
+    dcc.Dropdown(['TSLA','TRU','TWLO'], 'TSLA', id='ticker'),
     html.H3(children='Assets', style={'textAlign':'center'}),
     dcc.Graph(id='assets'),
     html.H3(children='EPS', style={'textAlign':'center'}),
     dcc.Graph(id='eps'),
     html.H3(children='Liabilities', style={'textAlign':'center'}),
     dcc.Graph(id='liabilities'),
-    html.H3(children='Expenses', style={'textAlign':'center'}),
-    dcc.Graph(id='expenses')
+    html.H3(children='Revenue', style={'textAlign':'center'}),
+    dcc.Graph(id='revenue')
 ])
 
 @callback( Output('assets', 'figure'), Input('ticker', 'value'))
@@ -39,12 +39,12 @@ def update_liabilities(ticker):
 
     return px.line(df, x='year', y='liabilities')
 
-@callback( Output('expenses', 'figure'), Input('ticker', 'value'))
+@callback( Output('revenue', 'figure'), Input('ticker', 'value'))
 def update_expenses(ticker):
 
     df=pd.read_csv(f'{ticker}.csv')
 
-    return px.line(df, x='year', y='expenses')
+    return px.line(df, x='year', y='revenue')
 
 
 
